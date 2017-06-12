f291a0ef9c267d3151ced33290a1d99abf80f717
benanderson89 <benanderson89@outlook.com>
2017-06-12 11:30:32 +0100

    Added BOM to IE BlobBuilder download.  Using single quotes as per proper JS guidelines.

--- Download.js
+++ Download.js
@@ -2,66 +2,71 @@
 // Trigger a document download
 // Ben Anderson 2016
 
-var DownloadJS = function (blob, filename, mimetype) {
+var DownloadJS = function (blob, filename, mimetype)
+{
     if (!blob) {
         throw {
-            name: "Argument Null Exception",
-            nameof: "blob",
-            description: "The supplied variable is null"
+            name: 'Argument Null Exception',
+            nameof: 'blob',
+            description: 'The supplied variable is null'
         }
     }
 
     if (!filename) {
         throw {
-            name: "Argument Null Exception",
-            nameof: "filename",
-            description: "The supplied variable is null"
+            name: 'Argument Null Exception',
+            nameof: 'filename',
+            description: 'The supplied variable is null'
         }
     }
 
     if (!mimetype) {
         throw {
-            name: "Argument Null Exception",
-            nameof: "mime",
-            description: "The supplied variable is null"
+            name: 'Argument Null Exception',
+            nameof: 'mime',
+            description: 'The supplied variable is null'
         }
     }
 
     if (!Array.isArray(blob)) {
         throw {
-            name: "Type Error",
-            nameof: "blob",
-            description: "Supplied data is not an array"
+            name: 'Type Error',
+            nameof: 'blob',
+            description: 'Supplied data is not an array'
         }
     }
 
-    //Without the parameter "\ufeff" the charset is changed after blob generation
-    //however it does not work in IE11, so wrap it in a try catch and use the obsolete blob builder
+    // Without the parameter '\ufeff' the charset is changed after blob generation
+    // however it does not work in IE11, so wrap it in a try catch
+    // '\ufeff' is the UTF-8 BOM
 
     var objectBlob;
 
     try{
-        objectBlob = new Blob(["\ufeff", blob], { type: mimetype });
+        objectBlob = new Blob(['\ufeff', blob], { type: mimetype });
     } catch (e)
     {
         var bb = new window.MSBlobBuilder();
+        bb.append(['\ufeff']);
         bb.append(blob);
         objectBlob = bb.getBlob();
     }
+    
 
     if (!navigator.msSaveOrOpenBlob) {
         var objUrl = URL.createObjectURL(objectBlob);
 
-        var a = document.createElement("a");
+        var a = document.createElement('a');
         a.download = filename;
         a.href = objUrl;
-        a.style.display = "none";
+        a.style.display = 'none';
         document.body.appendChild(a);
         a.click();
         /*In linux the link download element was removed too soon. this way would help.
          * For now this is the better approach I think.
          * */
-        setTimeout(function () {
+        setTimeout(function ()
+        {
             a.remove();
             a = undefined;
             URL.revokeObjectURL(objUrl);
